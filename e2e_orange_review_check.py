6d0a7933da80aa26d545d78b250acc5b8c24cba7
zoetaka38 <takayuki.kawazoe@corevice.com>
2026-06-24 11:45:12 +0200

    Revert "Add e2e_orange_review_check.py to verify Orange review loop (#2)"
    This reverts commit dd0a11c643d22b9efc79f7a90e38993814fa6f51.

--- e2e_orange_review_check.py
+++
@@ -1,3 +0,0 @@
-def build_greeting(name: str) -> str:
-    """Return a friendly greeting addressed to ``name``."""
-    return f"Hello, {name}!"
--- test_e2e_orange_review_check.py
+++
@@ -1,5 +0,0 @@
-from e2e_orange_review_check import build_greeting
-
-
-def test_build_greeting_world():
-    assert build_greeting("World") == "Hello, World!"
